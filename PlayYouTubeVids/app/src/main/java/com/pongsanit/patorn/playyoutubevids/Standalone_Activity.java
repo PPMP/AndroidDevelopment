2115f9ca6cde486304ca2e1fbefd51659a833b1b
PATORN PONGSANIT <PPMoss2537@gmail.com>
2016-07-28 19:00:16 -0400

    Play single youtube video

---
+++ PlayYouTubeVids/app/src/main/java/com/pongsanit/patorn/playyoutubevids/Standalone_Activity.java
@@ -0,0 +1,13 @@
+package com.pongsanit.patorn.playyoutubevids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Standalone_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standalone);
+    }
+}
--- PlayYouTubeVids/app/src/main/java/com/pongsanit/patorn/playyoutubevids/Youtube_Activity.java
+++ PlayYouTubeVids/app/src/main/java/com/pongsanit/patorn/playyoutubevids/Youtube_Activity.java
@@ -9,23 +9,26 @@ import com.google.android.youtube.player.YouTubePlayerView;
 public class Youtube_Activity extends YouTubeBaseActivity
     implements YouTubePlayer.OnInitializedListener
 {
-    @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
 
-    }
-
-    @Override
-    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
-    }
-
-    private String GOOGLE_API_KEY = "TBA";
-    private String YOUTUBE_VIDEO_ID = "TBA";
+    private String GOOGLE_API_KEY = "AIzaSyA7MUFjIq9ZeoEIzm_ELVesRT_RjWn9ttw";
+    private String YOUTUBE_VIDEO_ID = "kOkQ4T5WO9E";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
         YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player_view);
         youTubePlayerView.initialize(GOOGLE_API_KEY, this); //initialize a YouTubePlayer, which can be used to play videos and control video playback.
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if(!b) {
+            youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
     }
 }
