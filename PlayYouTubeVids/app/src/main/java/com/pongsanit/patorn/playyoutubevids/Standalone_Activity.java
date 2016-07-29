562c0d9791a5a2ed48bd36f7cbb9e8b269d3c620
PATORN PONGSANIT <PPMoss2537@gmail.com>
2016-07-28 20:00:28 -0400

    Youtube standalone player

--- PlayYouTubeVids/app/src/main/java/com/pongsanit/patorn/playyoutubevids/Standalone_Activity.java
+++ PlayYouTubeVids/app/src/main/java/com/pongsanit/patorn/playyoutubevids/Standalone_Activity.java
@@ -1,13 +1,47 @@
 package com.pongsanit.patorn.playyoutubevids;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Standalone_Activity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+public class Standalone_Activity extends AppCompatActivity
+        implements View.OnClickListener
+{
+
+    private String GOOGLE_API_KEY = "AIzaSyA7MUFjIq9ZeoEIzm_ELVesRT_RjWn9ttw";
+    private String YOUTUBE_VIDEO_ID = "kOkQ4T5WO9E";
+    private String PlAYLIST = "PLvFYFNbi-IBEzlv32QUnKOdmokcDrP3Oq";
+    private Button playVideo;
+    private Button playPlaylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standalone);
+        playVideo = (Button) findViewById(R.id.btnPlay);
+        playPlaylist = (Button) findViewById(R.id.btnPlayPlaylist);
+        playVideo.setOnClickListener(this);
+        playPlaylist.setOnClickListener(this);  //this points to the onClick function
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch(view.getId()) {
+            case R.id.btnPlay:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, GOOGLE_API_KEY, YOUTUBE_VIDEO_ID);
+                break;
+            case R.id.btnPlayPlaylist:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, PlAYLIST);
+                break;
+            default:
+        }
+        if(intent != null) {
+            startActivity(intent);      //good practice. If not, it crashes. starts youtube player
+        }
     }
 }
